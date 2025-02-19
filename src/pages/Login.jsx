90b52a4627a1659add26552cd66796a162ac442c
Carlos Sanabria <carlos.sanabria@upb.edu.co>
2025-02-19 17:51:48 -0500

    fix:New funcionalidad page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '@/components/shared/Input';
 import Button from '@/components/shared/Button';
 import Avatar from '@/components/shared/Avatar';
 
-
 const LoginPage = () => {
+    const [nickname, setNickname] = useState('');
+
     return (
-        <div className="max-w-md mx-auto px-4 py-30 flex flex-col items-center space-y-45 ">
-            <Avatar/>
-            <Input type="text" placeholder="NickName" />
-            <Button type="submit">Ingresar</Button>
+        <div className="py-7 px-7 h-screen">
+            <div className="bg-gray-400 h-full rounded-md max-w-md mx-auto px-4 py-10 flex flex-col items-center justify-around space-y-4">
+                <div className="text-center space-y-4">
+                <Avatar />
+                {nickname && <p className="text-white text-3xl font-semibold">{nickname}</p>}
+                </div>
+                <div className="flex flex-col space-y-5">
+                <Input 
+                    type="text" 
+                    placeholder="NickName" 
+                    value={nickname}
+                    onChange={(e) => setNickname(e.target.value)}
+                />
+                <Button type="submit">Ingresar</Button>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default LoginPage;
-
