1b003a3f3768f93f01f0693355d4638c2bd398ab
Carlos Sanabria <carlos.sanabria@upb.edu.co>
2025-02-22 21:33:41 -0500

    feat: new brach game, page, header

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import HeaderMenu from "../components/shared/HeaderMenu";
+
 const GamePage = () => {
     return (
-        <div className="text-purple-500">
-            Game
+        <div className="max-w-md mx-auto bg-gray-100 min-h-screen p-4">
+            <HeaderMenu/>
         </div>
     )
 }
