ccf990e1efad6346dcb96d6bd3665c5619507b62
JesusDiaz5678 <jesusa.diaz@upb.edu.co>
2025-02-24 12:06:57 -0500

    create new brain and add help

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 import LoginPage from "./pages/Login";
 import GameModesPage from "./pages/GameModes";
 import GamePage from "./pages/Game";
+import HelpPage from "./pages/Help";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/help" element={<HelpPage />} /> {/* Ruta del Help*/}
         <Route path="/game-modes" element={<GameModesPage />} />
         <Route path="/game/:name" element={<GamePage />} />
       </Routes>
---
+++ src/components/Wildcards.jsx
@@ -0,0 +1,7 @@
+export const Wildcard50 = () => {
+    return (
+        <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M4 5v14h16V5H4zm14 12H6V7h12v10zM8 9v2h8V9H8zm0 4v2h8v-2H8z" />
+        </svg>
+    )
+}
