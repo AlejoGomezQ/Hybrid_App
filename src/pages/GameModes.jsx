a32b49c6e16c7e1e3cd65ee34510f78618b37efb
JesusDiaz5678 <jesusa.diaz@upb.edu.co>
2025-02-26 06:30:41 -0500

    Merge pull request

--- src/pages/GameModes.jsx
+++ src/pages/GameModes.jsx
@@ -1,51 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react"
 import { IconJS, IconPython, IconSQL } from "../components/Icons"
 import TopicButton from "../components/TopicButton"
-import HeaderGameMode from "../components/HeaderGameMode";
+import HeaderGameMode from "../components/HeaderGameMode"
 
-import data from "../db/question.json";
-import { useEffect } from "react";
+import data from "../db/question.json"
 
 const GameModesPage = () => {
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([])
 
     // Cargar las categorías al montar el componente
     useEffect(() => {
-        setCategories(data.categorias);
-    }, []);
+        setCategories(data.categorias)
+    }, [])
 
     // Función para obtener el ícono correspondiente a cada categoría
     const getIconForCategory = (categoryName) => {
         switch (categoryName) {
             case "Frontend":
-                return <IconJS />;
+                return <IconJS />
             case "Backend":
-                return <IconPython />;
+                return <IconPython />
             case "Bases de datos":
-                return <IconSQL />;
+                return <IconSQL />
             default:
-                return null;
+                return null
         }
-    };
+    }
 
     return (
-        <div className="relative max-w-md mx-auto min-h-screen p-4 transition-all bg-gray-100">
-            <HeaderGameMode />
-            <h1 className="mb-6 text-xl font-bold text-center">Elige una categoria</h1>
-            <div className="h-full rounded-xl bg-gray-300">
-
-                <div className="flex flex-col space-y-1 gap-y-1 px-7 ">
-                    {categories.map((category) => (
-                        <TopicButton
-                            key={category.nombre}
-                            icon={getIconForCategory(category.nombre)}
-                            name={category.nombre}
-                        />
-                    ))}
+        <div className="h-screen bg-gradient-to-br from-purple-600 to-blue-500 font-sans flex flex-col">
+            <div className="flex-1 flex flex-col p-4">
+                <div className="max-w-md mx-auto w-full h-full flex flex-col bg-white/20 rounded-lg shadow-lg p-6">
+                    <HeaderGameMode />
+                    <h1 className="mb-6 text-2xl font-bold text-center text-white">Elige una categoria</h1>
+                    <div className="flex-1 flex flex-col gap-y-5">
+                        {categories.map((category) => (
+                            <TopicButton key={category.nombre} icon={getIconForCategory(category.nombre)} name={category.nombre} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default GameModesPage
