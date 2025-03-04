13eff79c726e4ce40774d0a566a0936fc7b7eeed
JesusDiaz5678 <jesusa.diaz@upb.edu.co>
2025-03-03 21:34:15 -0500

    Change in the color text from the ProgessPoints Component

--- src/components/ProgressPoints.jsx
+++ src/components/ProgressPoints.jsx
@@ -4,17 +4,14 @@ const ProgressPoints = ({ progress, startNumber, endNumber, isActive }) => {
     
     
     return (
-      <div className="flex flex-col items-center bg-white/20 p-2 rounded-lg w-full max-w-sm my-2">
+      <div className={`flex flex-col items-center p-2 rounded-lg w-full max-w-sm my-2 ${isActive ? 
+                      "bg-yellow-300" : "bg-white/20"}`}>
         <div className="relative w-full rounded-full h-6">
-          <div
-            className={`h-6 rounded-full transition-all duration-300 ${
-                isActive ? "bg-yellow-300" : ""}`}
-            style={{ width: `${progress}%` }}
-          ></div>
-          <div className="absolute top-0 left-2 text-white font-bold text-sm">
+        
+          <div className={`absolute top-0 left-2 font-bold text-sm ${isActive ? "text-black" : "text-white"}`}>
             {startNumber}
           </div>
-          <div className="absolute top-0 right-2 text-white font-bold text-sm">
+          <div className={`absolute top-0 right-2 font-bold text-sm ${isActive ? "text-black" : "text-white"}`}>
             {endNumber}
           </div>
         </div>
